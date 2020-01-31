e7fa6faa3c9832efe442a84b3311cec68e15b097
rails <mdubey@bestpeers.com>
2020-01-31 15:31:52 +0530

    added loading file

--- api/src/App.js
+++ api/src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import "./App.css";
+import Loading from "./loading";
 class App extends Component {
-constructor(props){
-super(props);
- this.state={
- 	items:[],
- 	isLoaded:false
- };
-}
+	constructor(props){
+	super(props);
+		this.state={
+		 	items:[],
+		 	isLoaded:false
+		};
+	}
 
 componentDidMount(){
 	fetch('https://jsonplaceholder.typicode.com/users')
@@ -18,46 +19,14 @@ componentDidMount(){
 				items: json,
 			})
 		});
+    }
+render(){
+	return(
+		<Loading isLoaded={this.state.isLoaded} items={this.state.items}/>
+
+	)
 }
 
-
-render(){
-
- var{ isLoaded, items } = this.state;
- 
- if(!isLoaded){
- 	return <div>Loading....</div>
- }
- 
- else{
-		   return(
-		 	<div className="App">
-		 	  	<div className="customers">
-
-                      			
-							<tr>
-						    <th>Name</th>
-						    <th>Email</th>
-						    <th>Username</th>
-						    </tr>
-	
-	                    {items.map(items =>(
-							<tr>
-						    <td>{items.name}</td>
-						    <td>{items.email}</td>
-						    <td>{items.username}</td>
-							</tr>
-                            
-                            ))};
-	 	  	
-		 	  	</div>	  	
-		 	   
-		 	</div>
-
-		      );
-
-          }
-       }
-   }
+}
 
 export default App; 
